3171f5929ad34610442e7488772928215b8ae82f
Esuabom Dijemeni <e.dijemeni@gmail.com>
2023-10-27 17:51:18 +0100

    Completed sql and yml file for order items and products

---
+++ lessons/models/staging/stg_ecommerce__products.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+WITH source AS (
+    SELECT *
+
+    FROM {{ source('thelook_ecommerce', 'products') }}
+)
+
+SELECT
+    id AS product_id,
+    cost,
+    retail_price,
+    department
+
+FROM source
