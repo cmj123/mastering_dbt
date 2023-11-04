9ab2a564c226835e734b31087c442d539e8d2384
Esuabom Dijemeni <e.dijemeni@gmail.com>
2023-11-04 07:01:05 +0000

    Created snapshot for distribution centers

---
+++ lessons/snapshots/snapshot__distribution_centers.sql
@@ -0,0 +1,21 @@
+{% snapshot snapshot__distribution_centers %}
+
+{{
+   config(
+       target_schema='dbt_snapshots',
+       unique_key='id',
+       strategy='check',
+       check_cols = ['name', 'latitude', 'longtitude']
+   )
+}}
+
+SELECT
+    id,
+    name,
+    latitude,
+    longitude
+
+FROM {{ source('thelook_ecommerce', 'distribution_centers') }}
+
+
+{% endsnapshot %}
