1b6ebd5023b85d030eeb181d94d0eadbd2cbb962
Esuabom Dijemeni <e.dijemeni@gmail.com>
2023-11-05 06:49:26 +0000

    Partitioned stg_ecommerce_events

--- lessons/models/staging/stg_ecommerce__events.sql
+++ lessons/models/staging/stg_ecommerce__events.sql
@@ -1,8 +1,14 @@
+
 {{
   config(
     materialized = 'incremental',
-    unique_key = 'event_id',
-    on_schema_change = 'sync_all_columns'
+    unique_key='event_id',
+    on_schema_change='sync_all_columns',
+    partition_by={
+            "field": "created_at",
+            "data_type": "timestamp",
+            "granularity": "day"
+        }
     )
 }}
 
